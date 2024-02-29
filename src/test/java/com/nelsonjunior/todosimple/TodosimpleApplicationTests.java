5d43c461b23520f92435f4116ade32da7a1bb617
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-02-29 09:45:33 -0300

    first commit

---
+++ src/test/java/com/nelsonjunior/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelsonjunior.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
