7b0f33ba90d6438bf94a0b8cf53338d5d3c68469
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-03-06 10:28:12 -0300

    Criacao do ProfileEnum

--- src/main/java/com/nelsonjunior/todosimple/models/User.java
+++ src/main/java/com/nelsonjunior/todosimple/models/User.java
@@ -1,7 +1,10 @@
 package com.nelsonjunior.todosimple.models;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,10 +20,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import com.nelsonjunior.todosimple.models.enums.ProfileEnum;
 
 @Entity
 @Table(name = User.TABLE_NAME)
@@ -57,6 +64,20 @@ public class User {
     @OneToMany(mappedBy = "user")
     @JsonProperty(access = Access.WRITE_ONLY) // quando buscar um usuário, nao mostrar as tasks dele
     private List<Task> tasks = new ArrayList<Task>();
+
+    @ElementCollection(fetch = FetchType.EAGER) // quando buscar usuário, vai buscar os perfis junto (admin ou user)
+    @JsonProperty(access = Access.WRITE_ONLY) // nao mostrar pro usuario qual e o perfil dele
+    @CollectionTable(name = "user_profile")
+    @Column(name = "profile", nullable = false)
+    private Set<Integer> profiles = new HashSet<>();
+
+
+    public Set<ProfileEnum> getProfiles() {
+        return this.profiles.stream().map(x-> ProfileEnum.toEnum(x)).collect(Collectors.toSet());
+    }
     
+    public void addProfile(ProfileEnum profileEnum) {
+        this.profiles.add(profileEnum.getCode());
+    }
 
 }
--- src/main/java/com/nelsonjunior/todosimple/services/UserService.java
+++ src/main/java/com/nelsonjunior/todosimple/services/UserService.java
@@ -1,18 +1,25 @@
 package com.nelsonjunior.todosimple.services;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.nelsonjunior.todosimple.models.User;
+import com.nelsonjunior.todosimple.models.enums.ProfileEnum;
 import com.nelsonjunior.todosimple.repositories.UserRepository;
 import com.nelsonjunior.todosimple.services.exceptions.DataBindingViolationException;
 import com.nelsonjunior.todosimple.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder; //criptar o password
     
     @Autowired
     private UserRepository userRepository;
@@ -30,6 +37,8 @@ public class UserService {
     @Transactional // utilizado para caso for salvar algo no banco, por exemplo, create e update
     public User create(User obj){
         obj.setId(null); // garantir que ta criando um novo
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword())); // senha criptografada
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet())); // garantir que quando o usuario for criado, for criado com o perfil de numero 2
         obj = this.userRepository.save(obj);
         return obj;
     }
@@ -39,6 +48,7 @@ public class UserService {
     public User update(User obj){
         User newObj = findById(obj.getId()); // pra ve se existe esse usuario 
         newObj.setPassword(obj.getPassword());
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword())); // senha criptografada
         return this.userRepository.save(newObj);
     }
 
